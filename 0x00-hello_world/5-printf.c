375eef694e346a3c446dbb60a0074ef23354dae4
EREEK71 <ericelesho@gmail.com>
2022-06-03 03:01:52 -0700

    me

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,15 @@
+/*
+ * File: 5-printf.c
+ * Auth: EREEK71
+ */
+#include <stdio.n>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is 
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
