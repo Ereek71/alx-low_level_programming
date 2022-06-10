c6304f973aaf2d3fc67d28a1ebd8f5be6ec150c9
EREEK71 <ericelesho@gmail.com>
2022-06-10 08:55:33 -0700

    C

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar: a function
+ * @u: an integer
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c)
+{
+	return _putchar(char c);
+}
