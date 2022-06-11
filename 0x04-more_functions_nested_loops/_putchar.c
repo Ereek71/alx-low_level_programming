3c9b09bef7663b0226dab5d2d7b69b80fe9eafd4
EREEK71 <ericelesho@gmail.com>
2022-06-11 03:27:41 -0700

    C

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,9 +5,10 @@
  * _putchar: a function
  * @u: an integer
  * Return: Always 0 (Success)
+ * on error, -1 is returned, and error is set appropriately.
  */
 
 int _putchar(char c)
 {
-	return _putchar(char c);
+	return _putchar(write(1, &c, 1));
 }
