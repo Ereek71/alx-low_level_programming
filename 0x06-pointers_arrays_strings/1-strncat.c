f5ebda43f19402555012b0170aebc9772590bece
EREEK71 <ericelesho@gmail.com>
2022-06-16 17:40:38 -0700

    Task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most an inputted number of bytes from src.
+ * _strncat - a function that concatenates two strings
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
